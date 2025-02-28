c40b579ecdf185d4516a77946e523a7d287909bb
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 06:54:01 +0000

    Compted 5th Exercise

---
+++ Exercise5-Sequence_Indexing.py
@@ -0,0 +1,9 @@
+# Question: Complete the script so that it prints out the second item of the list.
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# b
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[1])

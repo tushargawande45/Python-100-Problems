e304986365b371c09aeb5f1642ded16684223e1a
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 07:07:32 +0000

    Completed 10th Exercise

---
+++ Exercise10-Sequence_Item_Picking.py
@@ -0,0 +1,9 @@
+# Question: Complete the script so that it prints out a list slice containing letters a, c, e, g, and i. 
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['a', 'c', 'e', 'g', 'i'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[::2])

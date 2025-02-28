9c6bd954549d20fc2e7cbc7e418eab8c2ae29a69
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 06:56:24 +0000

    Completed 6th Exercise

---
+++ Exercise6-Sequence_Slicing.py
@@ -0,0 +1,9 @@
+# Question: Please complete the script so that it prints out a list slice containing items d , e , and f .
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['d', 'e', 'f'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[3:6])

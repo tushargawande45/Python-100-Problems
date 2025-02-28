1386465e9518543d978a1c2cb84764a0fbbe9ea5
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 06:50:10 +0000

    Solved 4th Exercise

---
+++ Exercise4-TypeError.py
@@ -0,0 +1,12 @@
+# Question: Fix the last line so that it outputs the sum of 1 and 2. Please do not change the first two lines, only the last one.
+
+# a = "1"
+# b = 2
+# print(a + b)
+# Expected output: 
+
+# 3 
+
+a = "1"
+b = 2
+print(int(a) + b)

e3d8110a5f96a999040a06c6c15b0df88862cba3
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 07:04:03 +0000

    Completed 9th Exercise

---
+++ Exercise9-Negative_Slicing.py
@@ -0,0 +1,9 @@
+# Question: Complete the script so that it prints out a list slice containing the last three items of the list letters .
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output:
+
+# ['h', 'i', 'j'] 
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+print(letters[-3:])

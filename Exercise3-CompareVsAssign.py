62bb85d354238dd34c4c65b697685ca43045eb27
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-02-28 06:47:29 +0000

    Done with 3rd Exercise

---
+++ Exercise3-CompareVsAssign.py
@@ -0,0 +1,8 @@
+# Question: Executing the code will throw an error. Can you explain why?
+
+# a = 1
+# b = 2
+# print(a == b)
+# print(b == c)
+
+# reason : Variable c is not defined and here they used comparison operator also

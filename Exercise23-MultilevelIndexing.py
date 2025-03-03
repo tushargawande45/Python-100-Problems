d88a3adc59d352715f8685d70039aca9d6e8a704
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-03 10:24:45 +0000

    Solved Ex.23

---
+++ Exercise23-MultilevelIndexing.py
@@ -0,0 +1,15 @@
+# Question: Access the third value of key b  from the dictionary.
+
+# from pprint import pprint
+ 
+# d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+# Expected output: 
+
+# 13 
+
+
+from pprint import pprint
+ 
+d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+print(d['b'][2])
+

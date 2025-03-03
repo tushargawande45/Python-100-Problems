8d879b7044f7d36f46a05149d7e2f23950a6f917
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-03 12:36:59 +0000

    Solved Ex.30

---
+++ Exercise30-Arguments.py
@@ -0,0 +1,9 @@
+# Question:  Why do you get an error, and how would you fix it?
+
+# def foo(a=2, b):
+#     return a + b
+
+# Hint: Always put non-default parameters first, followed by default ones.
+
+def foo(b,a=2):
+     return a + b

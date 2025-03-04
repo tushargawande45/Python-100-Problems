f4e8f3b93a8ed391a684cea2a085722bd2572466
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-04 08:58:34 +0000

    Solved Ex.39

---
+++ Exercise39-AttributeError.py
@@ -0,0 +1,11 @@
+# Question: The script is supposed to output the cosine of angle 1 radian, but instead, it is throwing an error. Please fix the code so that it prints out the expected output.
+
+# import math
+# print(math.cosine(1))
+# Expected output:
+
+# 0.5403023058681397
+
+import math
+print(dir(math))
+print(math.cos(1))

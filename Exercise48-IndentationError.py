994ea98fa4c399b9563f9f06221151e954865b49
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 08:57:04 +0000

    Solved Ex.48

---
+++ Exercise48-IndentationError.py
@@ -0,0 +1,15 @@
+# Question: The script is supposed to print out the letter "e" if the letter is in the string "Hello," but it doesn't. Please try to fix the script.
+
+# for letter in "Hello":
+#     if letter == "e":
+#     print(letter)
+# Expected output:
+# e 
+
+
+# Solution:The line under an if  statement should always be indented.
+for letter in "Hello":
+    if letter == "e":
+        print(letter)
+
+

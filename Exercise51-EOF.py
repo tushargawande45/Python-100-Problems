013b78a431fd65a21b4b3626211efc88d5b6fab1
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 09:09:15 +0000

    Solved Ex.51

---
+++ Exercise51-EOF.py
@@ -0,0 +1,7 @@
+# Question: The code produces an error. Please understand the error and try to fix it.
+
+# print(type("Hey".replace("ey","i")[-1])
+
+
+# solution: An open bracket always needs a closing bracket.
+print(type("Hey".replace("ey","i")[-1]))

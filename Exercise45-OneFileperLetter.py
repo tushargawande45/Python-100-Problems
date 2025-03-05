ec8402bbbeac36b36aaa1c4f5e5b5db76d4f39af
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 06:36:40 +0000

    Solved Ex.45

---
+++ Exercise45-OneFileperLetter.py
@@ -0,0 +1,13 @@
+# Question: Please create a script that generates 26 text files named a.txt, b.txt, and so on up to z.txt. Each file should contain a letter reflecting its filename. So, a.txt will contain letter a, b.txt will contain letter b, and so on.
+
+import string
+
+def func(filename):
+    with open(f"{filename}.txt","w") as file:
+        file.write(f"{filename}")
+        file.close()
+
+for letter in string.ascii_lowercase:
+    func(letter)
+    
+

7867ffa0401f8eb04fdbcaa330d056c4af55c837
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 11:57:24 +0000

    Solved Ex.61

---
+++ Exercise61-TimedPrinter.py
@@ -0,0 +1,16 @@
+# Question: Create a program that prints out Hello  every two seconds.
+
+# Expected output: 
+
+# ...
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# ...
+import time
+while True:
+    print("Hello")
+    time.sleep(2.0)

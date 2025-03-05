26e7b8a2de85ca48680369c4c341bfa3c78d96e4
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 12:06:43 +0000

    Solved Ex.62

---
+++ Exercise62-ProgressiveTimedPrinter.py
@@ -0,0 +1,22 @@
+# Question: Create a program that, once executed the program prints Hello  instantly first, then it prints it after 1 second, then after 2, 3, 4, and so on the interval increases between prints.
+
+# Expected output: 
+
+# ...
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# ...
+
+import time
+
+print("Hello")
+counter = 1
+while True:
+    time.sleep(counter)
+    print("Hello")
+    counter+=1
+    

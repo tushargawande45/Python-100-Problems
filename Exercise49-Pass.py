1971eb9d4e8b136d65a7bf6d5e4cd0a7d5a54396
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-05 09:00:41 +0000

    Solved Ex.49

---
+++ Exercise49-Pass.py
@@ -0,0 +1,8 @@
+# Question: The code is supposed to get some input from the user, but instead, it produces an error. Please try to understand the error and then fix it.
+
+# pass = input("Please enter your password: ") 
+
+# Note: Please use raw_input  instead of input if you are on Python 2. For Python 3 input  is fine.
+
+# solution: You cannot use reserved keywords for variable names.
+pass1 = input("Please enter your password: ") 

ac9147f9892ccd7df0cbdac6389ee2f36b66c226
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-06 06:06:43 +0000

    Solved Ex.67

---
+++ Exercise67-AdvancedTranslator.py
@@ -0,0 +1,21 @@
+# Question: Create an English to Portuguese translation program.
+
+# The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source. In addition, try to return the message, "We couldn't find that word!" when the user enters a word that is not in the dictionary.
+
+# d = dict(weather = "clima", earth = "terra", rain = "chuva") 
+
+# Expected output: 
+
+# Enter word: hello
+# That word doesn't exist!
+
+def translator(inputLang):
+    if inputLang in d:
+        print(d[inputLang])
+    else:
+        print("That word doesn't exist!")
+    
+
+d = dict(weather = "clima", earth = "terra", rain = "chuva")
+inputLang = input("Enter word: ")
+translator(inputLang)

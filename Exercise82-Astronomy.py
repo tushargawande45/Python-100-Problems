1ddd02bdaf157c4a53be0eb86e5f20d25e6cb7a0
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-07 09:45:24 +0000

    Solved Ex.82

---
+++ Exercise82-Astronomy.py
@@ -0,0 +1,13 @@
+# Question: Use Python to calculate the distance in kilometers between Jupiter and Sun on January 1, 1230. 
+
+# Expected output: 
+
+# 758085657.5026425
+
+
+import ephem
+jupiter = ephem.Jupiter()
+jupiter.compute('1230/1/1')
+distance_au_units = jupiter.sun_distance
+distance_km = distance_au_units * 149597870.691
+print(distance_km)

62aaf4375b2bbb5eefba54c8d366a0d6996e38aa
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-07 12:21:26 +0000

    Solved Ex.87

---
+++ Exercise87-AddMissingData.py
@@ -0,0 +1,17 @@
+# Add the missing items to the file 
+
+# checklist = ["Portugal", "Germany", "Spain"]
+
+checklist = ["Portugal", "Germany", "Spain"]
+checklist = [i + "\n" for i in checklist]
+
+
+with open("countries_missing.txt", "r") as file:
+    content = file.readlines()
+
+updated_list = sorted(checklist + content)
+
+with open("countries_missing_fixed.txt","w") as file:
+    for i in updated_list:
+        file.write(i)
+

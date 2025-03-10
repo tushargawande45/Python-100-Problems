bf3bd8c13f8f925a6a847ffd93bae4d24ba9da6a
Tushar Gawande <124259935+tushargawande45@users.noreply.github.com>
2025-03-10 10:33:23 +0000

    Solved Ex.93

---
+++ Exercise96-FileWriter.py
@@ -0,0 +1,11 @@
+# write a program that asks the user to submit text repeatedly
+# the program closed when the user submits CLOSE 
+
+file = open("user_data.txt","a+") 
+while True:
+    line = input("Enter a value: ")
+    if line == "CLOSE":
+        file.close()
+        break
+    else:
+        file.write(line + "\n")
